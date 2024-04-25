82bdc48c701cc71512411ee06caae2b2886ae49f
Robbie <robbie331@>
2024-04-25 17:40:21 +0900

    Level 1 - 3
    입력을 받아 사칙연산 기능 구현
    0으로 나눌 경우 오류문 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+//        1. Scanner 를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
+//                - 양의 정수는 각각 하나씩 전달 받습니다.
+//                - 양의 정수는 적합한 타입으로 선언한 변수에 저장합니다.
         Scanner sc = new Scanner(System.in);
 
         System.out.println("첫 번째 숫자를 입력하세요.");
@@ -14,17 +17,22 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요."); // +, -, *, /
         // sc.next 가 받은 String 타입의 문자열중 charAt(i) i번 째의 값을 char 형태로 저장 ( i는 0부터 시작 )
         char operator = sc.next().charAt(0);
+        // 계산을 받을 int 선언
+        int result = 0;
 
         // 0으로 나눌 수 없기 때문에 0이 입력되면 오류문 출력
         if (secondNum == 0) {
             System.out.println("나눗셈 연산에서 분모(두번째 정수)는 0이 입력될 수 없습니다.");
+            result = firstNum; // 0으로 나눌경우 첫번째 정수값 출력
         } else {
-            switch (operator) {
-                case '+': System.out.println(firstNum + secondNum); break;
-                case '-': System.out.println(firstNum - secondNum); break;
-                case '*': System.out.println(firstNum * secondNum); break;
-                case '/': System.out.println((double)(firstNum / secondNum)); break;
-            }
+            result = switch (operator) {
+                case '+' -> firstNum + secondNum;
+                case '-' -> firstNum - secondNum;
+                case '*' -> firstNum * secondNum;
+                case '/' -> firstNum / secondNum;
+                default -> result;
+            };
         }
+        System.out.println("결과 = " + result);
     }
 }
