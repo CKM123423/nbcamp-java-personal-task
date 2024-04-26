f45a484429a8e922ada013c3ba221121ee5c6be5
Robbie <robbie331@>
2024-04-26 20:49:51 +0900

    Level 2 - 7
    원의 넓이를 구하는 메서드 구현
    사칙연산의 결과값과 원의 넓이의 결과값을 다른곳에 저장
    사칙연산과 동일한 기능을 수행하도록하는 메서드을 구현
    App 에서 직접 원하는 기능을 매번 선택하여 exit 를 입력하기전까지 무한으로 구동하도록 구현

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,32 +10,58 @@ public class App {
 
         // exit 입력창에서 exit 를 입력받을때까지 무한반복 (어디서든 exit 를 쓰면 종료되는 문제점 해결)
         while (true) {
-            System.out.println("첫 번째 숫자를 입력하세요.");
-            int firstNum = sc.nextInt(); // 첫번째 변수를 int 값으로 저장
+            System.out.println("어떤 기능을 원하십니까?");
+            System.out.println("1. 사칙연산 ('1' 을 입력해 시작하세요.)");
+            System.out.println("2. 원의 넓이 계산('2' 를 입력해 시작하세요.)");
+            int choice = sc.nextInt(); // 어떤 작업을 수행할지에 선택에 대한 조건으로 쓰기위해 선언
 
-            System.out.println("두 번째 숫자를 입력하세요.");
-            int secondNum = sc.nextInt(); // 두번째 변수를 int 값으로 저장
+            if (choice == 1) {
+                System.out.println("첫 번째 숫자를 입력하세요.");
+                int firstNum = sc.nextInt(); // 첫번째 변수를 int 값으로 저장
 
-            System.out.println("사칙연산 기호를 입력하세요."); // +, -, *, /
-            // sc.next 가 받은 String 타입의 문자열중 charAt(i) i번 째의 값을 char 형태로 저장 ( i는 0부터 시작 )
-            char operator = sc.next().charAt(0);
+                System.out.println("두 번째 숫자를 입력하세요.");
+                int secondNum = sc.nextInt(); // 두번째 변수를 int 값으로 저장
 
-            System.out.println("결과 = " + calc.calculate(firstNum, operator, secondNum));
+                System.out.println("사칙연산 기호를 입력하세요."); // +, -, *, /
+                // sc.next 가 받은 String 타입의 문자열중 charAt(i) i번 째의 값을 char 형태로 저장 ( i는 0부터 시작 )
+                char operator = sc.next().charAt(0);
 
-            System.out.println("가장 먼저 저장된 결과값 : [" + calc.getResultList().getFirst() + "] "); // 가장 먼저 저장된 결과값
-            System.out.println("가장 먼저 저장된 결과를 삭제하시겠습니까? (remove 입력시 삭제)");
-            if (sc.next().equals("remove")) {
-                System.out.println("\n가장 먼저 저장된 결과값 [" + calc.getResultList().getFirst() + "] 이 삭제되었습니다.");
-                calc.removeResult(); // 가장 처음 저장된 결과값을 삭제 Calculator 에서 만든 메서드로 접근해서 수정
+                System.out.println("결과 = " + calc.calculate(firstNum, operator, secondNum));
+
+                System.out.println("가장 먼저 저장된 결과값 : [" + calc.getArithmeticList().getFirst() + "] "); // 가장 먼저 저장된 결과값
+                System.out.println("가장 먼저 저장된 결과를 삭제하시겠습니까? (remove 입력시 삭제)");
+                if (sc.next().equals("remove")) {
+                    System.out.println("\n가장 먼저 저장된 결과값 [" + calc.getArithmeticList().getFirst() + "] 이 삭제되었습니다.");
+                    calc.removeArithmeticList(); // 가장 처음 저장된 결과값을 삭제 Calculator 에서 만든 메서드로 접근해서 수정
+                }
+
+                System.out.println("현재까지 저장된 모든 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if (sc.next().equals("inquiry")) {
+                    System.out.println("현재 저장된 배열의 값들 : ");
+                    calc.inquiryArithmeticList(); // inquiry 입력시 List 의 값들을 읽어와 프린트 해주는 메서드
+                }
+                // 현재 저장된 결과값의 총 개수
+                System.out.println("\n현재 저장된 배열의 총 개수 : " + calc.getArithmeticList().size());
+
+            } else if (choice == 2) {
+                // 위의 사칙연산과 동일한 내용을 수행하도록 구현
+                // 새로운 CircleArea 리스트를 만들어서 사칙연산과 따로따로 저장되도록 구현
+                System.out.println("원의 반지름을 입력해주세요");
+                System.out.println("결과 = " + calc.calculateCircleArea(sc.nextDouble()));
+                System.out.println("가장 먼저 저장된 결과값 : [" + calc.getCircleArea().getFirst() + "] ");
+                System.out.println("가장 먼저 저장된 결과를 삭제하시겠습니까? (remove 입력시 삭제)");
+                if (sc.next().equals("remove")) {
+                    System.out.println("\n가장 먼저 저장된 결과값 [" + calc.getCircleArea().getFirst() + "] 이 삭제되었습니다.");
+                    calc.removeCircleArea();
+                }
+                System.out.println("현재까지 저장된 모든 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if (sc.next().equals("inquiry")) {
+                    System.out.println("현재 저장된 배열의 값들 : ");
+                    calc.inquiryCircleArea(); // inquiry 입력시 List 의 값들을 읽어와 프린트 해주는 메서드
+                }
+                // 현재 저장된 결과값의 총 개수
+                System.out.println("\n현재 저장된 배열의 총 개수 : " + calc.getCircleArea().size());
             }
-
-            System.out.println("현재까지 저장된 모든 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if (sc.next().equals("inquiry")) {
-                System.out.println("현재 저장된 배열의 값들 : ");
-                calc.inquiryList(); // inquiry 입력시 List 의 값들을 읽어와 프린트 해주는 메서드
-            }
-            // 현재 저장된 결과값의 총 개수
-            System.out.println("\n현재 저장된 배열의 총 개수 : " + calc.getResultList().size());
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             // while 문을 무한반복으로 바꾼뒤 해당 부분에서 exit 가 입력될경우 while 문 탈출
             if (sc.next().equals("exit")) {
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 public class Calculator {
 
     // 연산 결과 저장할 리스트 선언과 생성 private 로 다른 클래스들에서 정보에 접근하지 못하게 함
-    private ArrayList<Integer> resultList = new ArrayList<>();
+    // 변수명을 바꾸고 사칙연산과 원의 넓이를 저장할 필드 각각 선언
+    private ArrayList<Integer> ArithmeticList = new ArrayList<>();
+    private ArrayList<Double> CircleArea = new ArrayList<>();
 
     // 원래 자동으로 생성되던 기본 생성자를 명시화하고 생성자가 생성될때마다 새로운 ArrayList 를 만들어 그곳에 데이터를 저장
+    // 클래스가 호출될때마다 각 리스트들을 초기화
     public Calculator() {
-        this.resultList = new ArrayList<>();
+        this.ArithmeticList = new ArrayList<>();
+        this.CircleArea = new ArrayList<>();
     }
+
     public double calculate(int firstNum, char operator, int secondNum) throws BadInputException {
         int result; // 결과값을 반환하기 위한 지역변수 선언
         if (operator == '+') {
@@ -29,29 +34,70 @@ public class Calculator {
             // 사칙연산 이외의 값이 입력 되었다면 익셉션을 발생시키고 오류문 출력
             throw new BadInputException("입력이 잘못되었습니다. " + operator + " 대신 사칙연산자에 해당되는 연산자를 넣어주세요. (+, -, *, /)");
         }
-        setResultList(result); // setter 를 사용해서 리스트에 계산 결과를 저장
+        setArithmeticList(result); // setter 를 사용해서 리스트에 계산 결과를 저장
         return result;
     }
 
+    // 사칙연산
     // 데이터를 받아오는 입구 ArrayList 가 private 가 되면서 다른 클래스에서 접근 및 조회할 수 없는 문제를 해결하기 위해서 생성
-    public ArrayList<Integer> getResultList() {
-        return resultList;
+    public ArrayList<Integer> getArithmeticList() {
+        return ArithmeticList;
     }
 
+    // 사칙연산
     // 데이터를 주입하는 입구 calculate 가 계산과 배열에 입력 모두를 수행하고 있어서 setter 를 만들어 책임분산
-    public void setResultList(int result) {
-        this.resultList.add(result);
+    public void setArithmeticList(int result) {
+        this.ArithmeticList.add(result);
     }
 
+    // 사칙연산
     // 리스트에 처음 입력된 값을 삭제하기 위한 메서드
-    public void removeResult() {
-        this.resultList.removeFirst();
+    public void removeArithmeticList() {
+        this.ArithmeticList.removeFirst();
     }
 
+    // 사칙연산
     // 결과값이 저장된 List 를 조회해 프린트해주는 메서드
-    public void inquiryList() {
-        for (Integer integer : getResultList()) {
+    public void inquiryArithmeticList() {
+        for (Integer integer : getArithmeticList()) {
             System.out.print("[" + integer + "] ");
         }
     }
+
+    // 원의 넓이
+    // 원의 넓이를 계산하고 저장하는 메서드
+    public double calculateCircleArea(double radius) {
+        double result;
+        result =Math.PI * radius * radius; // 원의 넓이 구하는 공식
+        setCircleArea(result); // CircleArea 에 결과값 추가
+        return result;
+    }
+
+    // 원의 넓이
+    // CircleArea 의 값을 조회하는데에 사용
+    public ArrayList<Double> getCircleArea() {
+        return CircleArea;
+    }
+
+    //원의 넓이
+    // CircleArea 에 값을 넣어주는데에 사용
+    public void setCircleArea(double radius) {
+        this.CircleArea.add(radius);
+    }
+
+    // 원의 넓이
+    // 첫번째 저장값 삭제
+    public void removeCircleArea() {
+        this.CircleArea.removeFirst();
+    }
+
+
+    // 원의 넓이
+    // 원의 넓이가 저장된 CircleArea 의 데이터를 시스템에 출력
+    public void inquiryCircleArea() {
+        for (Double doubles : getCircleArea()) {
+            System.out.print("[" + doubles + "] ");
+        }
+    }
+
 }
