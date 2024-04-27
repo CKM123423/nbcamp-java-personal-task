8b94545930c3381369a2ac66c9a00801a876314c
Robbie <robbie331@>
2024-04-28 01:07:15 +0900

    Level 2 - 9
    사칙연산의 각각의 기능들을 다른 클래스에 따로따로 넣어 ArithmeticCalculator 클래스의 책임을 낮춤(SRP)
    
    ArithmeticCalculator 에서 각각의 연산기능을 가진 클래스들을 인스턴스화하여 클래스 내부에서 연산이 가능하도록 구현

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -2,20 +2,33 @@ package calculator;
 
 public class ArithmeticCalculator extends Calculator {
 
+    // ArithmeticCalculator 클래스가 생성되었을때 각각의 연산기능을 가진 클래스들을 인스턴스화 (포함관계)
+    // 클래스 내부에서만 계산을 진행하고 연산기능이 바뀌는 것을 막기위한 private final 로 선언
+    private final AddOperator addOperator;
+    private final SubtractOperator subtractOperator;
+    private final MultiplyOperator multiplyOperator;
+    private final DivideOperator divideOperator;
+
+    // 생성자가 호출될때 초기화를 해주면서 각각의 클래스들이 최대한 온전한 상태로 기능을 가져올 수 있도록 함
+    //
+    public ArithmeticCalculator() {
+        addOperator = new AddOperator();
+        subtractOperator = new SubtractOperator();
+        multiplyOperator = new MultiplyOperator();
+        divideOperator = new DivideOperator();
+    }
+
+    // 계산에 필요한 연산자와 숫자들을 매개변수로 받고 연산자를 통해 각각의 연산기능을 실행할 수 있도록 구현
     public double calculate(int firstNum, char operator, int secondNum) throws BadInputException{
-        int result; // 결과값을 반환하기 위한 지역변수 선언
+        double result; // 결과값을 반환하기 위한 지역변수 선언
         if (operator == '+') {
-            result = firstNum + secondNum;
+            result = addOperator.add(firstNum, secondNum);
         } else if (operator == '-') {
-            result = firstNum - secondNum;
+            result = subtractOperator.subtract(firstNum, secondNum);
         } else if (operator == '*') {
-            result = firstNum * secondNum;
+            result = multiplyOperator.multiply(firstNum, secondNum);
         } else if (operator == '/') {
-            if (secondNum == 0) {
-                // 나누기 연사자일때 두번째 입력값이 0이면 익셉션을 발생시키고 오류문 출력
-                throw new BadInputException("0 으로 나눌 수 없습니다.");
-            }
-            result = firstNum / secondNum;
+            result = divideOperator.divide(firstNum, secondNum);
         } else {
             // 사칙연산 이외의 값이 입력 되었다면 익셉션을 발생시키고 오류문 출력
             throw new BadInputException("입력이 잘못되었습니다. " + operator + " 대신 사칙연산자에 해당되는 연산자를 넣어주세요. (+, -, *, /)");
